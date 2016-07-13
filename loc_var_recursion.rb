4a6fea10fdd3843149083f45a9cd4d9dd0483440
Michael Ryan <m.ryan1@icloud.com>
2016-07-13 10:31:38 -0400

    Ch 5 recursion ex

---
+++ loc_var_recursion.rb
@@ -0,0 +1,17 @@
+class C
+  def x(value_for_a, recurse=false)
+    a = value_for_a
+    print "Here's the inspect-string for 'self': "
+    puts self.object_id
+    puts "And here's a: "
+    puts a.object_id
+    if recurse
+      puts "Calling myself (recursion)..."
+      x("Second value for a")
+      puts "Back after recursion; here's a: "
+      puts a.object_id
+    end
+  end
+end
+c = C.new
+c.x("First value for a", true)
